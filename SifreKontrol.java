87ccd05b2126e9da86983c777bb11d3e50bc934e
gulzeki96 <80572516+gulzeki96@users.noreply.github.com>
2021-07-26 14:11:10 +0300

    Create SifreKontrol.java

---
+++ SifreKontrol.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+
+class SifreKontrol{
+  static String kullaniciAdi = "patika";
+  static String sifre = "java123";
+  public static void main(String[] args){
+  
+    String username, password;
+    Scanner sc = new Scanner(System.in);
+    Scanner scChar = new Scanner(System.in);
+    int girisFlag = 0;
+    char c;
+    while(girisFlag == 0){
+      System.out.print("Kullanici Adi : "); username = sc.nextLine();
+      System.out.print("Sifre         : "); password = sc.nextLine();
+    
+     if(username.equals(kullaniciAdi) && password.equals(sifre)){
+       System.out.println("Giris Yaptiniz!");
+       girisFlag = 1;
+     }
+     else{
+       System.out.println("Giris Bilgileri Hatali");
+       System.out.println("Sifrenizi Sifirlamak İstiyor musunuz? ((e)Evet/(h)Hayir) "); c = scChar.next().charAt(0);
+       if(c == 'e'){
+         while(true){
+           System.out.println("Yeni sifre giriniz: "); password = sc.nextLine();
+           if(sifre.equals(password)){
+             System.out.println("gecersiz sifre!"); continue;
+           }
+           else{
+             //sifre = '\0';
+             sifre = password;
+             break;
+           }
+         }
+       } 
+     }
+  }
+ }
+}
